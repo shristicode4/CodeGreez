90b1d392b623807df9027a8bbad73dad027bb5f1
Shristicode4 <shristikaadhikari88@gmail.com>
2024-07-28 10:52:23 +0530

    home

--- codegreez/src/pages/Home.js
+++ codegreez/src/pages/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper"> form</div>
+    </div>
+  );
 };
 
 export default Home;
