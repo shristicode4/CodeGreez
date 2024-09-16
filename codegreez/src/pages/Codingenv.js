677acefa0255e7d672288838c8ba1a24910664a7
Shristicode4 <shristikaadhikari88@gmail.com>
2024-09-17 04:45:01 +0530

    editor logo name

--- codegreez/src/pages/Codingenv.js
+++ codegreez/src/pages/Codingenv.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "../components/Clients";
 
 const Codingenv = () => {
-  return <div></div>;
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Shristi" },
+    { socketId: 2, username: "Kashis" },
+  ]);
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoimage" src="/cglogo.jpg" alt="logo" />
+          </div>
+          <h3> connected </h3>
+          <div className="clientList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="editorWrap"> Editor goes here</div>
+    </div>
+  );
 };
 
 export default Codingenv;
